72894857efaabfcb119ace2eeccc3c0ba33e61ed
Jordan Schupbach <jordans1882@gmail.com>
2018-08-18 18:12:00 -0600

    Removed redundant folders and added zathurarc

--- .config_/cmus/playlist.pl
+++
@@ -1,3 +0,0 @@
-/home/jordan/Audio/Santigold/Chasing Shadows.flac
-/home/jordan/Audio/Johann Sebastian Bach/Unaccompanied Cello Suite No  3 in C Major  BWV 1009: Sarabande.flac
-/home/jordan/Audio/Modest Mouse/Missed the Boat.flac
